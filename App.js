a4db3fc559ec9f75dfdd19eefab0681a728be3f5
React-native <yarishka007@gmail.com>
2023-12-02 17:29:22 +0200

    Layout of the basic components homework 2

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import HomeScreen from './src/screens/home/screens/HomeScreen';
+import ProductCard from './src/screens/home/screens/ProductCard';
 
 export default function App() {
   return (
-    <HomeScreen />
+    <ProductCard />
   )
 }
