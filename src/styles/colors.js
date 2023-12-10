4efcb96476ce0a802fb70b641ee587d7f9e5bede
React-native <yarishka007@gmail.com>
2023-12-10 17:28:57 +0200

    HomeWork 3: add PLP, custom button

---
+++ src/styles/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    mainColor: '#000000',
+    backgroundColor: '#ffffff',
+    buttonColor: '#3498db',
+    buttonPressColor: '#00FFFF',
+    modalBackgroundColor: "rgba(0, 0, 0, 0.5)",
+    shadowColor: "#000",
+};
+
+export default colors;
